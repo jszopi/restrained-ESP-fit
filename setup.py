e296054ae9ce13773a603978d7698c834961d854
Jan Szopinski <jszopi@gmail.com>
2020-05-27 21:34:58 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import subprocess
+
+subprocess.run("make").check_returncode()
+subprocess.run("./restrained_ESP_fit").check_returncode()
+
+config = {
+    'name': 'restrained_ESP_fit',
+    'version': '2.4.1',
+    'description': 'Fitting partial charges to molecular Electrostatic Potential field',
+    'author': 'Jan Szopinski',
+    'author_email': 'jszopi@users.noreply.github.com',
+    'url': 'https://github.com/jszopi/restrained_ESP_fit',
+    'packages': [],
+    'license': 'GPLv3',
+    'entry_points': {
+        'console_scripts': "restrained_ESP_fit=restrained_ESP_fit:main"
+    },
+}
+
+setup(**config)
