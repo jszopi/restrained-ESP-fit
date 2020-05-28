e8c6cdac159fbbe150234793caab5aa261ec84b8
Jan Szopinski <jszopi@gmail.com>
2020-05-28 05:11:13 +0100

    Remove debugging leftover

--- restrained_ESP_fit/resp_wrapper.py
+++ restrained_ESP_fit/resp_wrapper.py
@@ -7,6 +7,4 @@ import subprocess
 
 def main():
     resp_binary = pkg_resources.resource_filename(__name__, "resp")
-    with open(f"temp+{datetime.datetime.now()}", 'w') as fh:
-        fh.write(f"\n{resp_binary}")
     subprocess.run([resp_binary, *sys.argv[1:]]).check_returncode()
