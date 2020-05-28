802b65fa4eac839cc8dc3fd4dfba7591d45617f7
Jan Szopinski <jszopi@users.noreply.github.com>
2020-05-28 11:17:20 +0100

    Add setup.py (#5)
    Compile the Fortran source during Python installation with setup.py and make it available through a Python script.

---
+++ restrained_ESP_fit/resp_wrapper.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import datetime
+import pkg_resources
+import sys
+import subprocess
+
+def main():
+    resp_binary = pkg_resources.resource_filename(__name__, "resp")
+    subprocess.run([resp_binary, *sys.argv[1:]]).check_returncode()
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+import subprocess
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+subprocess.run(["make"]).check_returncode()
+subprocess.run(["mv", "resp.o", "resp", "restrained_ESP_fit"]).check_returncode()  # Always returns 0
+
+config = {
+    'name': 'restrained_ESP_fit',
+    'version': '2.4.1',
+    'description': 'Fitting partial charges to molecular Electrostatic Potential field',
+    'long_description': long_description,
+    'long_description_content_type': 'text/markdown',
+    'maintainer': 'Jan Szopinski',
+    'maintainer_email': 'jszopi@users.noreply.github.com',
+    'url': 'https://github.com/jszopi/restrained_ESP_fit',
+    'license': 'GPLv3',
+    # Probably not the best way of making the `resp` binary accessible:
+    'packages': ["restrained_ESP_fit"],
+    'package_data': {"restrained_ESP_fit": ["resp"]},
+    'entry_points': {
+        'console_scripts': ["restrained_ESP_fit=restrained_ESP_fit.resp_wrapper:main"],
+    },
+}
+
+setup(**config)
