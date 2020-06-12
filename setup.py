990c84b7497ae09cf504f2497068e6c866814a3e
Jan Szopinski <jszopi@gmail.com>
2020-06-12 13:34:42 +0100

    Fix console script

--- setup.py
+++ setup.py
@@ -21,9 +21,10 @@ config = {
     'maintainer_email': 'jszopi@users.noreply.github.com',
     'url': 'https://github.com/jszopi/restrained_ESP_fit',
     'license': 'GPLv3',
+    'packages': ["restrained_ESP_fit"],
     'data_files': [(".", ["resp"])],
     'entry_points': {
-        'console_scripts': ["restrained_ESP_fit=resp_wrapper:main"],
+        'console_scripts': ["restrained_ESP_fit=restrained_ESP_fit.resp_wrapper:main"],
     },
     'distclass': BinaryDistribution
 }
