d3664d8202c2f0aaa74fd137462c172a8598a81d
Jan Szopinski <jszopi@gmail.com>
2020-06-12 13:54:29 +0100

    Fix accessing binary resp

--- restrained_ESP_fit/resp_wrapper.py
+++ restrained_ESP_fit/resp_wrapper.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 import datetime
-import pkg_resources
+import os
 import sys
 import subprocess
 
 def main():
-    resp_binary = pkg_resources.resource_filename(__name__, "resp")
+    resp_binary = os.path.join(sys.prefix, 'resp')
     subprocess.run([resp_binary, *sys.argv[1:]]).check_returncode()
