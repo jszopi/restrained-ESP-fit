dff01f7de06924bdaa4eb65d2cfd02f65b917148
Jan Szopinski <jszopi@users.noreply.github.com>
2020-06-29 20:49:07 +0100

    CI: Publish to test PyPI (#8)
    In Travis CI, build the sdist and two wheels (manylinux2014 and macosx_10_15, both 64bit) to the test PyPI. When the commit is git-tagged, the artifacts will also be deployed to test PyPI.
    
    `libgfortran` and `libquadmath` are linked statically, other libraries, which AFAIUI can be assumed to exist on any of the supported systes out of the box, are linked dynamically.
    
    Includes a few changes to `setup.py`:
    * only compile during the `build` step rather than whenever the script is invoked. Also adds
    * do not include the compiled `resp` in the sdist
    * derive package version from git tags

--- Makefile
+++ Makefile
@@ -1,19 +1,29 @@
+# Fortran compiler. Only gfortran is compatible with the flags used here and
+# the cross-platform build environments in Travis CI. Specifically, the
+# invocation via gcc is due to an issue with static linking on macOS.
+FC = $(or $(GCC_OVERRIDE),gcc) 
 
-# Fortran compiler: gfortran, g77, ifort, pgf77...
-FC = gfortran
-
-FLAGS = -c -O2
+FLAGS = -c -O2 -v
 # -mcmodel=medium
 # option for gfortran
 # allows handling of a larger number of MEP on 64 bit system
 # FLAGS = -c -mcmodel=medium -O2
 
-OBJS= resp.o  
+OBJS= resp.o
 SRCS= resp.f
 LIB= shared_variables.h
 
-resp:	$(OBJS) 
-	$(FC) $(OBJS) -o resp
+# A trick to statically link selected libraries, which works on macOS,
+# since the -Wl,Bstatic/dynamic approach only works with the GNU `ld`.
+# See https://gcc.gnu.org/bugzilla/show_bug.cgi?id=46539#c3
+# and https://stackoverflow.com/a/5583245
+vpath %.a $(VPATH_DIR)
+.LIBPATTERNS = lib%.a lib%.dylib lib%.so
+STATICLIBS = -lgfortran -lquadmath
+
+resp:	$(OBJS) $(STATICLIBS)
+	# -lm is needed on Linux per https://gcc.gnu.org/bugzilla/show_bug.cgi?id=46539#c3
+	$(FC) $^ -lm -o resp
 
 $(OBJS): $(SRCS) $(LIB)
 	$(FC) $(FLAGS) $(SRCS)
--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 from setuptools import setup
+import distutils.command.build
 import subprocess
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-subprocess.run(["make"]).check_returncode()
-subprocess.run(["mv", "resp.o", "resp", "restrained_ESP_fit"]).check_returncode()  # Always returns 0
+
+class build_(distutils.command.build.build):
+
+    def run(self):
+        print("Running build")
+        subprocess.run(["make"]).check_returncode()
+        subprocess.run(["mv", "resp", "restrained_ESP_fit"]).check_returncode()
+        distutils.command.build.build.run(self)
+
 
 config = {
     'name': 'restrained_ESP_fit',
-    'version': '2.4.1',
     'description': 'Fitting partial charges to molecular Electrostatic Potential field',
     'long_description': long_description,
     'long_description_content_type': 'text/markdown',
@@ -17,12 +24,29 @@ config = {
     'maintainer_email': 'jszopi@users.noreply.github.com',
     'url': 'https://github.com/jszopi/restrained_ESP_fit',
     'license': 'GPLv3',
-    # Probably not the best way of making the `resp` binary accessible:
     'packages': ["restrained_ESP_fit"],
     'package_data': {"restrained_ESP_fit": ["resp"]},
+    # Hacky? Causes the `resp` binary to be included in bdist_wheel but not in sdist
+    'include_package_data': True,
     'entry_points': {
         'console_scripts': ["restrained_ESP_fit=restrained_ESP_fit.resp_wrapper:main"],
     },
+    'cmdclass': {'build': build_},
+    'use_scm_version': True,
+    'setup_requires': ["setuptools_scm"],
+    'python_requires': ">=3",
+    'classifiers': [
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Scientific/Engineering :: Chemistry",
+    ],
+    'keywords': ["restrained molecular electrostatic potential ESP fitting RESP atomic partial charges"]
 }
 
 setup(**config)
