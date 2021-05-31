a0e5fe1152c502cba09dd68e25998ced4998659a
Jan Szopinski <jszopi@gmail.com>
2021-05-31 21:12:18 +0100

    Explanatory comments

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ config = {
         'console_scripts': ["restrained_ESP_fit=restrained_ESP_fit.resp_wrapper:main"],
     },
     'cmdclass': {'build': build_},
+    # Removing the local version as PyPI doesn't allow it
     'use_scm_version': {"local_scheme": "no-local-version"},
     'setup_requires': ["setuptools_scm"],
     'python_requires': ">=3",
